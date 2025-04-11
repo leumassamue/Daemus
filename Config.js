5b981defc115ff37d5d5346115eece4854c1e4a2
leumassamue <sakinlolu26@gmail.com>
2025-04-12 00:06:49 +0100

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [7477468702], // ganti jadi id mu
+    botToken: "7904511983:AAHBVGSC3Ci1XXcONJhy2698FD2gk63IoHw", //isi make token bot mu
+}
+
+module.exports = global;
